3c04bb3cdcd87ae42132248e6f2c73811e29d37b
lujeremy <jeremy.lu@stern.nyu.edu>
2019-06-30 14:16:51 -0400

    Initial setup to fetch from API

---
+++ src/components/workouts.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Workouts = ({ workouts }) => {
+  return (
+      <div>
+          <h1>Workouts List</h1>
+          {workouts.map((workout) => (
+              <div className="card">
+                  <div className="card-body">
+                      <h5 className="card-title">{workout.name}</h5>
+                      <h6 className="card-subtitle mb-2 text-muted">Sets: {workout.sets}, Reps: {workout.reps}</h6>
+                      <p className="card-text">Weight: {workout.weight}</p>
+                  </div>
+              </div>
+          ))}
+      </div>
+  )
+};
+
+export default Workouts;
